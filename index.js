c93dea726c5057b7aabfc8b33e240b7651a3bf5f
Romaniric-Basuyaux <romainric.basuyaux@campus-centre.fr>
2025-01-31 09:49:55 +0100

    Added POST request

--- index.js
+++ index.js
@@ -18,8 +18,42 @@ let produits =[
 
 app.get("/produits",(req, res) =>{
   res.json(produits);
-
 })
+//POST
+// Ajouter un nouveau produits
+app.post('/produits', (req, res) => {
+    const newProduits = req.body;
+
+   /* if (!newProduits || !newProduits.id || !newProduits.name) {
+        return res.status(400).json({ message: 'Invalid produits data' });
+    }*/
+
+    produits.push(newProduits);
+    res.status(201).json(newProduits);
+});
+
+//PUT
+//Mettre à jour un produit existant
+
+app.put('/produits/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    let produit = produits.find(produit => produit.id === id);
+    produit.nom = req.body.nom;
+    produit.prix = req.body.prix;
+    produit.quantite = req.body.quantite;
+    res.status(200).json(produit);
+});
+
+//DELETE
+//SUPP UN PRODUIT
+
+app.delete('/produits/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    let produit = produits.find(produit => produit.id === id);
+    produits.splice(produits.indexOf(produit), 1);
+    res.status(200).json(produits);
+});
+
 
 
 
